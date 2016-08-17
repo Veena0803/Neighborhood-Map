1699344a78b2538fd749c19c6a8b54fbc79bc993
Veena0803 <iyerveena08@gmail.com>
2016-08-17 11:49:48 -0400

    Updated changes as per review comments

--- js/app.js
+++ js/app.js
@@ -77,53 +77,75 @@ var model = {
     }]
 };
 
-//API Call with OAuth signature reqired to access Yelp
-var oAuthCall = function(i) {
+class Location {
+    constructor(store) {
+        this.title = store.title;
+        this.yelp_code = store.yelp_code;
+        this.marker = new google.maps.Marker({
+            position: store.location,
+            map: map,
+            animation: google.maps.Animation.DROP,
+        });
+        this.marker.addListener('click', () => {
+            // the "arrow" function creates a scopeless anonymous function,
+            // kind of like function() {}
+            // the benefit is that it is scopeless, so you don't need
+            // var self = this
+            this.showInfo();
+        })
+    }
+    
+        getYelpInfo() {
+        function nonce_generate() {
+            return (Math.floor(Math.random() * 1e12).toString());
+        }
 
-    function nonce_generate() {
-        return (Math.floor(Math.random() * 1e12).toString());
+        var parameters = {
+            oauth_consumer_key: 'qutEa0DTMawXiFmnOEb41Q',
+            oauth_token: 'vFRwvEtznjhipFwSjXdr7LsEFfArGE0q',
+            oauth_nonce: nonce_generate(),
+            oauth_timestamp: Math.floor(Date.now() / 1000),
+            oauth_signature_method: 'HMAC-SHA1',
+            oauth_version: '1.0',
+            callback: 'cb'
+        };
+
+        var encodedSignature = oauthSignature.generate('GET', this.yelp_code, parameters, 'ysgxjLNiWjvRXqlbAjfqv2oL5Fg', 'XU8NxJ_vlejrThWh4nf22hS9tVA');
+        parameters.oauth_signature = encodedSignature;
+
+        $.ajax({
+            url: this.yelp_code,
+            data: parameters,
+            cache: true,
+            dataType: 'jsonp',
+            timeout: 1000
+        }).done((results) => {
+            this.result = results;
+            this.phone = results.phone;
+            this.rating = results.rating_img_url;
+            this.review_count = results.review_count;
+            this.review = results.snippet_text;
+            this.contentString = '<h2 id="firstHeading" class="firstHeading">' + this.title + '</h2>' + '<h3 id="rating" class="rating">Rating:</h3>' + '<img src=' + this.rating + '>' + '<h3 id="reviews" class="reviews">Reviews:</h3>' + '<div id="bodyContent">' + this.review + '<br>' + '<h3 id="review_count" class="review_count">Review Count:</h3>' + '<div id="bodyContent">' + this.review_count + '<br>'+ '<h3 id="phone" class="phone">Phone:</h3>' + '<div id="bodyContent">' + this.phone + '<br>' + '<br>' + 'Information provided by Yelp' + '</div>';
+            //Incase API Call fails
+        }).fail(() => {
+            this.contentString = 'Yelp API Failed'
+        }).always(() => {
+            infowindow.setContent(this.contentString);
+            infowindow.open(map, this.marker);
+            this.bounce();
+        });
     }
 
-    var yelp_URL = model.pizzaStore[i].yelp_code;
+    bounce() {
+        this.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(() => {
+            this.marker.setAnimation(null);
+        }, 1400)
+    }
 
-    var parameters = {
-        oauth_consumer_key: 'qutEa0DTMawXiFmnOEb41Q',
-        oauth_token: 'vFRwvEtznjhipFwSjXdr7LsEFfArGE0q',
-        oauth_nonce: nonce_generate(),
-        oauth_timestamp: Math.floor(Date.now() / 1000),
-        oauth_signature_method: 'HMAC-SHA1',
-        oauth_version: '1.0',
-        callback: 'cb'
-    };
-
-    var encodedSignature = oauthSignature.generate('GET', yelp_URL, parameters, 'ysgxjLNiWjvRXqlbAjfqv2oL5Fg', 'XU8NxJ_vlejrThWh4nf22hS9tVA');
-    parameters.oauth_signature = encodedSignature;
-    var infoWindowData = {
-        url: yelp_URL,
-        data: parameters,
-        cache: true,
-        dataType: 'jsonp',
-        success: function(results) {
-            //Binds API results to model data
-            model.pizzaStore[i].result = results;
-            model.pizzaStore[i].phone = results.phone;
-            model.pizzaStore[i].rating = results.rating_img_url;
-            model.pizzaStore[i].review_count = results.review_count;
-            model.pizzaStore[i].review = results.snippet_text;
-        },
-        error: function() {
-            //Error call when API fails
-            error();
-        }
-    };
-
-    // Send AJAX query via jQuery library.
-    $.ajax(infoWindowData);
-};
-
-// Pop up to indicate in case the API call fails
-function error(){
-  window.alert("The API callback has failed, please try again");
+    showInfo() {
+        this.getYelpInfo();
+    }
 }
 
 //Google Maps implementation
@@ -140,13 +162,10 @@ function initMap() {
     
     //Sets the maximum width of the infowindow
     infowindow = new google.maps.InfoWindow({
-        maxWidth: 350
+        maxWidth: 500
     });
-    
-    //Yelp API call
-    for (var i = 0; i < model.pizzaStore.length; i++) {
-        oAuthCall(i);
-    }
+
+    var modelLength = model.pizzaStore.length;
     
     //Binds knockout bindings to ViewModel
     var vm = new viewModel();
@@ -158,47 +177,11 @@ var viewModel = function() {
 
     var self = this;
     self.filterText = ko.observable('');
-    self.pizzaStore = ko.observableArray(model.pizzaStore);
-    self.pizzaStore().forEach(function(store) {
-
-        //Creates markers for each object in the self.pizzaStore observable array
-        var marker = new google.maps.Marker({
-            position: store.location,
-            title: store.title,
-            map: map,
-            animation: google.maps.Animation.DROP,
-        });
-
-        //Assigns the marker info to store.marker
-        store.marker = marker;
-
-        var googleEvent = google.maps.event;
-
-        //Sets the markers and title info on map using a closure
-        googleEvent.addListener(marker, 'click', (function(marker, map, infowindow) {
-            return function() {
-                //Sets the content of the individual infowindows
-                var contentString = '<h2 id="firstHeading" class="firstHeading">' + store.title + '</h2>' + '<h3 id="rating" class="rating">Rating:</h3>' + '<img src=' + store.rating + '>' + '<h3 id="reviews" class="reviews">Reviews:</h3>' + '<div id="bodyContent">' + store.review + '<br>' + '<h3 id="review_count" class="review_count">Review Count:</h3>' + '<div id="bodyContent">' + store.review_count + '<br>'+ '<h3 id="phone" class="phone">Phone:</h3>' + '<div id="bodyContent">' + store.phone + '<br>' + '<br>' + 'Information provided by Yelp' + '</div>';
-                infowindow.setContent(contentString);
-                infowindow.open(map, marker);
-
-                //Animates google marker when clicked
-                if (marker.getAnimation() !== null) {
-                    marker.setAnimation(null);
-                } else {
-                    marker.setAnimation(google.maps.Animation.BOUNCE);
-                    stopAnimation(marker);
-                }
-            };
-        })(marker, map, infowindow));
-
-        //Animates the markers for a specified time
-        function stopAnimation(marker) {
-            setTimeout(function() {
-                marker.setAnimation(null);
-            }, 1000);
-        }
-    });
+    self.pizzaStore = ko.observableArray();
+    model.pizzaStore.forEach(function(store) {
+      var s = new Location(store);
+      self.pizzaStore.push(s);
+});
 
     //Provides the search functionality
     self.search = ko.computed(function() {
